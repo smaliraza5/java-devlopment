024e7b222d67281111c516039eb65aa1012e7e52
Syed Muhammad Ali Raza <53118857+smaliraza5@users.noreply.github.com>
2022-08-15 01:11:12 +0500

    loop update

--- table.java
+++ table.java
@@ -1,18 +1,27 @@
 import java.util.*;
 public class table {
-    public static void main( String args[]){
+    // public static void main( String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int num = sc.nextInt();
+    //     System.out.println(num * 1 + " ");
+    //     System.out.println(num * 2 + " ");
+    //     System.out.println(num * 3 + " ");
+    //     System.out.println(num * 4 + " ");
+    //     System.out.println(num * 5 + " ");
+    //     System.out.println(num * 6 + " ");
+    //     System.out.println(num * 7 + " ");
+    //     System.out.println(num * 8 + " ");
+    //     System.out.println(num * 9 + " ");
+    //     System.out.println(num * 10 + " ");
+
+    // }
+    // table by for loop 
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        System.out.println(num * 1 + " ");
-        System.out.println(num * 2 + " ");
-        System.out.println(num * 3 + " ");
-        System.out.println(num * 4 + " ");
-        System.out.println(num * 5 + " ");
-        System.out.println(num * 6 + " ");
-        System.out.println(num * 7 + " ");
-        System.out.println(num * 8 + " ");
-        System.out.println(num * 9 + " ");
-        System.out.println(num * 10 + " ");
+        int n= sc.nextInt();
+        for(int i=1; i<11; i++){
+            System.out.println(i*n);
+        }
 
     }
 }
