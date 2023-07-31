da29914f67ede6896f98881dffa598d7285a6ed9
Syed Muhammad Ali Raza <53118857+smaliraza5@users.noreply.github.com>
2023-07-31 21:56:14 +0500

    uncommented all the code wich was commented  out
    new commit -- uncomment all the commented code to view it better

--- loops.java
+++ loops.java
@@ -1,32 +1,32 @@
 import java.util.*;
 public class loops {
      public static void main(String args[]){
-        // Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
-        // for(int i=0; i<=n; i++){
-        //     if (i%2==0){
-        //         System.out.println(i);
-        //     }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<=n; i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
 
-        // } 
+        } 
 ///////////////////////////////////////////////////////input driven program from user
 
-    //   Scanner sc = new Scanner(System.in);
-    //   int input;
-    //   do {
-    //     int marks= sc.nextInt();
-    //     if (marks >= 90 && marks <=100){
-    //         System.out.println("This is Good");
-    //     }else if(marks >=60 && marks <= 89){
-    //         System.out.println("This is also good");
-    //     }else if(marks >=0 && marks <=59){
-    //         System.out.println("This is also good as well");
-    //     }else{
-    //         System.out.println("Invalid number");
-    //     } 
-    //     System.out.println("want to continue (yes(1) or (no(0)");
-    //     input = sc.nextInt();
-    //   }  while (input==1);
+      Scanner sc = new Scanner(System.in);
+      int input;
+      do {
+        int marks= sc.nextInt();
+        if (marks >= 90 && marks <=100){
+            System.out.println("This is Good");
+        }else if(marks >=60 && marks <= 89){
+            System.out.println("This is also good");
+        }else if(marks >=0 && marks <=59){
+            System.out.println("This is also good as well");
+        }else{
+            System.out.println("Invalid number");
+        } 
+        System.out.println("want to continue (yes(1) or (no(0)");
+        input = sc.nextInt();
+      }  while (input==1);
       ///////////////////////////////////////////////////////////////
       //NUMBER IS PRIME OR NOT ////
       //////////////////////////////
--- patterns.java
+++ patterns.java
@@ -1,48 +1,48 @@
 public class patterns {
     public static void main(String args[]){
-        ///////////rectangle///////////////
-        // int n = 4;
-        // int m = 5;
-        // // outer loop
-        // for(int i=1; i<=n; i++){
-        //     //inner loop
-        //     for(int j=1; j<=m; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
+        /////////rectangle///////////////
+        int n = 4;
+        int m = 5;
+        // outer loop
+        for(int i=1; i<=n; i++){
+            //inner loop
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
 
-        // }
-        /////////hollow rectangle////////////////////////
-        // int n =4;
-        // int m = 5;
-        // for(int i = 1 ; i<=4; i++){
-        //     for(int j=1; j<=5; j++){
-        //         if(i == 1 || j == 1|| i == n || j == m ){
-        //             System.out.print("*");
-        //         }else{
-        //             System.out.print(" ");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
+        }
+        ///////hollow rectangle////////////////////////
+        int n =4;
+        int m = 5;
+        for(int i = 1 ; i<=4; i++){
+            for(int j=1; j<=5; j++){
+                if(i == 1 || j == 1|| i == n || j == m ){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
-        //////////////////////////////////////////////////
-        // int n = 5;
-        // for (int i = 1; i<=5 ; i++){
-        //     for(int j = 1 ; j <=i; j++ ){
-        //         System.out.print("*");
-        //     }System.out.println();
-        // }
-        ///////////////////////////above pattern invert//////////
-        // int n = 5;
-        // for( int i =n ; i>=1; i--){
-        //     for(int j=1; j<=i ; j++){
-        //         System.out.print("*");
+        ////////////////////////////////////////////////
+        int n = 5;
+        for (int i = 1; i<=5 ; i++){
+            for(int j = 1 ; j <=i; j++ ){
+                System.out.print("*");
+            }System.out.println();
+        }
+        /////////////////////////above pattern invert//////////
+        int n = 5;
+        for( int i =n ; i>=1; i--){
+            for(int j=1; j<=i ; j++){
+                System.out.print("*");
 
-        //     }
-        //     System.out.println();
-        // }
-        ///////////////pattern rotatted by 180degree /////////////////////////////
+            }
+            System.out.println();
+        }
+        /////////////pattern rotatted by 180degree /////////////////////////////
         int n = 5;
         for(int i=1; i<=n; i++){ 
 
